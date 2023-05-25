0964b6ba627e396ded1b59ef88fb321d4d2199df
Nurgali <nrknrps@gmail.com>
2023-05-25 11:26:55 +0600

    minor changes 0.4

--- src/components/PizzaBlock/index.jsx
+++ src/components/PizzaBlock/index.jsx
@@ -1,11 +1,11 @@
-import React from "react";
-import { useDispatch, useSelector } from "react-redux";
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
-import { addItem } from "../../redux/slices/cartSlice";
+import { addItem } from '../../redux/slices/cartSlice';
 
 function PizzaBlock({ id, title, price, imageUrl, sizes, types }) {
   const dispatch = useDispatch();
-  const typeNames = ["тонкое", "традиционное"];
+  const typeNames = ['тонкое', 'традиционное'];
   const [activeSize, setActiveSize] = React.useState(0);
   const [activeType, setActiveType] = React.useState(0);
 
@@ -29,15 +29,15 @@ function PizzaBlock({ id, title, price, imageUrl, sizes, types }) {
         <div className="pizza-block__selector">
           <ul>
             {types.map((type, index) => {
-              return (
-                <li
-                  key={type}
-                  className={activeType === index ? "active" : ""}
-                  onClick={() => setActiveType(index)}
-                >
-                  {typeNames[type]}
-                </li>
-              );
+              // return (
+              //   <li
+              //     key={type}
+              //     className={activeType === index ? "active" : ""}
+              //     onClick={() => setActiveType(index)}
+              //   >
+              //     {typeNames[type]}
+              //   </li>
+              // );
             })}
           </ul>
           <ul>
@@ -45,9 +45,8 @@ function PizzaBlock({ id, title, price, imageUrl, sizes, types }) {
               return (
                 <li
                   key={size}
-                  className={activeSize === index ? "active" : ""}
-                  onClick={() => setActiveSize(index)}
-                >
+                  className={activeSize === index ? 'active' : ''}
+                  onClick={() => setActiveSize(index)}>
                   {size} см.
                 </li>
               );
@@ -56,24 +55,20 @@ function PizzaBlock({ id, title, price, imageUrl, sizes, types }) {
         </div>
         <div className="pizza-block__bottom">
           <div className="pizza-block__price">{price} тг</div>
-          <button
-            onClick={onClickAdd}
-            className="button button--outline button--add"
-          >
+          <button onClick={onClickAdd} className="button button--outline button--add">
             <svg
               width="12"
               height="12"
               viewBox="0 0 12 12"
               fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
+              xmlns="http://www.w3.org/2000/svg">
               <path
                 d="M10.8 4.8H7.2V1.2C7.2 0.5373 6.6627 0 6 0C5.3373 0 4.8 0.5373 4.8 1.2V4.8H1.2C0.5373 4.8 0 5.3373 0 6C0 6.6627 0.5373 7.2 1.2 7.2H4.8V10.8C4.8 11.4627 5.3373 12 6 12C6.6627 12 7.2 11.4627 7.2 10.8V7.2H10.8C11.4627 7.2 12 6.6627 12 6C12 5.3373 11.4627 4.8 10.8 4.8Z"
                 fill="white"
               />
             </svg>
             <span>Добавить</span>
-            <i>{"0"}</i>
+            <i>{'0'}</i>
           </button>
         </div>
       </div>
